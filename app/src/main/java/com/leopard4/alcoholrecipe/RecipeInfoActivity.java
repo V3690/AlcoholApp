9c05b227413c6f1c68e7edd775a2ff9afe3b96fb
kimjieun990525 <120348534+kimjieun990525@users.noreply.github.com>
2023-03-22 11:53:39 +0900

    중복 코드 제거

--- app/src/main/java/com/leopard4/alcoholrecipe/RecipeInfoActivity.java
+++ app/src/main/java/com/leopard4/alcoholrecipe/RecipeInfoActivity.java
@@ -178,6 +178,18 @@ public class RecipeInfoActivity extends AppCompatActivity {
                         Glide.with(RecipeInfoActivity.this)
                                 .load(recipeOne.getImgUrl())
                                 .into(imgUrl);
+
+                        // 레시피를 작성한 사람이라면 수정버튼을 보여주기 위해서
+                        // myrecipeAdapter 에서 받은 userId
+                        getAdapterUserId = getIntent().getIntExtra("userId", 0);
+//                    Log.i("접속한유저ID", getAdapterUserId+"");
+                        userId = recipeOne.getUserId();
+//                    Log.i("유저ID테스트", userId+"");
+                        if (userId == getAdapterUserId){
+                            btnEdit.setVisibility(View.VISIBLE);
+                        }
+
+
                         if (recipeOne.getIsLike() == 1) {
                             imgRecipeLike.setImageResource(R.drawable.baseline_favorite_24);
                         }
@@ -190,17 +202,12 @@ public class RecipeInfoActivity extends AppCompatActivity {
                             recipeOne.setAlcoholType("");
                         }
                         if (recipeOne.getEngTitle() == null) {
-                            recipeOne.setEngTitle("영어이름이 없어요");
+                            recipeOne.setEngTitle("");
                         }
                         if (recipeOne.getIntro() == null) {
                             recipeOne.setIntro("");
                         }
-                        if (recipeOne.getContent() == null) {
-                            recipeOne.setContent("");
-                        }
-                        if (recipeOne.getIngredient() == null) {
-                            recipeOne.setIngredient("");
-                        }
+
                         // 닉네임 뿌려주기
                         if (recipeOne.getUserId() == 1) {
                             txtUserId.setText("주인장의시크릿");
@@ -215,8 +222,9 @@ public class RecipeInfoActivity extends AppCompatActivity {
                         } else if (recipeOne.getPercent() == 3) {
                             txtPercent.setText("강");
                         } else {
-                            txtPercent.setText("알수없음");
+                            txtPercent.setText("비밀");
                         }
+
 
                         txtRecipeTitle.setText(recipeOne.getTitle()+"");
                         txtLikeCnt.setText(recipeOne.getLikeCnt()+"");
@@ -227,76 +235,6 @@ public class RecipeInfoActivity extends AppCompatActivity {
                         txtIngredient.setText(recipeOne.getIngredient()+ "");
 
                         likeCnt = recipeOne.getLikeCnt();
-
-
-                    // 레시피를 작성한 사람이라면 수정버튼을 보여주기 위해서
-                    // myrecipeAdapter 에서 받은 userId
-                    getAdapterUserId = getIntent().getIntExtra("userId", 0);
-//                    Log.i("접속한유저ID", getAdapterUserId+"");
-                    userId = recipeOne.getUserId();
-//                    Log.i("유저ID테스트", userId+"");
-                    if (userId == getAdapterUserId){
-                        btnEdit.setVisibility(View.VISIBLE);
-                    }
-
-
-                    // glide로 이미지 뿌려주기
-                    Glide.with(RecipeInfoActivity.this)
-                            .load(recipeOne.getImgUrl())
-                            .into(imgUrl);
-
-                    // recipeOnd에 값이 없는게 있을때 처리
-                    if (recipeOne.getImgUrl() == null) {
-                        recipeOne.setImgUrl("");
-                    }
-                    if (recipeOne.getAlcoholType() == null) {
-                        recipeOne.setAlcoholType("");
-                    }
-                    if (recipeOne.getEngTitle() == null) {
-                        recipeOne.setEngTitle("");
-                    }
-                    if (recipeOne.getIntro() == null) {
-                        recipeOne.setIntro("");
-                    }
-                    // 닉네임 뿌려주기
-                    if (recipeOne.getUserId() == 1) {
-                        txtUserId.setText("주인장의시크릿");
-                    } else {
-                        txtUserId.setText(recipeOne.getNickname());
-                    }
-                    // 도수 뿌려주기
-                    if (recipeOne.getPercent() == 1) {
-                        txtPercent.setText("약");
-                    } else if (recipeOne.getPercent() == 2) {
-                        txtPercent.setText("중");
-                    } else if (recipeOne.getPercent() == 3) {
-                        txtPercent.setText("강");
-                    } else {
-                        txtPercent.setText("비밀");
-                    }
-
-                    // todo: 나중에 지워야댐 근데 레시피 db에는 주종이라는게 없는데 ??
-                    // 답변 : 주종 제외시키기로 해놓고 기획 수정을 안했네요...
-                    // 오븐에 화면 수정된거 반영했습니다. (3/21)
-                    Log.i("레시피 정보1", response.body().toString());
-                    Log.i("레시피 정보2", recipeOne.toString()+"");
-                    Log.i("레시피 정보3", recipeOne.getImgUrl()+"");
-                    Log.i("레시피 정보4", recipeOne.getTitle()+"");
-                    Log.i("레시피 정보5", recipeOne.getLikeCnt()+"");
-                    Log.i("레시피 정보6", recipeOne.getPercent()+"");
-                    Log.i("레시피 정보7", recipeOne.getAlcoholType()+"");
-                    Log.i("레시피 정보8", recipeOne.getUserId()+"");
-                    Log.i("레시피 정보9", recipeOne.getEngTitle()+"");
-                    Log.i("레시피 정보10", recipeOne.getIntro()+"");
-                    Log.i("레시피 정보11", recipeOne.getContent()+"");
-                    Log.i("레시피 정보12", recipeOne.getIngredient()+ "");
-                    Log.i("레시피 정보13", recipeOne.getNickname()+ "");
-
-
-//                    imgUrl.setImageResource(Integer.parseInt(recipeOne.getImgUrl()));
-                    txtRecipeTitle.setText(recipeOne.getTitle()+"");
-                    txtLikeCnt.setText(recipeOne.getLikeCnt()+"");
-                    txtAlcoholType.setText(recipeOne.getAlcoholType()+"");
 
 
                     }
