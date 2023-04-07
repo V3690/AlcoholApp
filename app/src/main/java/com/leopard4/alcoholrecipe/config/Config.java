5bdb19dc727df941ed1a0fe5f3001f250ce24326
kimjieun990525 <120348534+kimjieun990525@users.noreply.github.com>
2023-04-07 12:15:52 +0900

    .
    .

---
+++ app/src/main/java/com/leopard4/alcoholrecipe/config/Config.java
@@ -0,0 +1,15 @@
+package com.leopard4.alcoholrecipe.config;
+
+public class Config {
+
+    // 서버 주소
+    public static final String DOMAIN = "https://ntg4o5zh7b.execute-api.ap-northeast-2.amazonaws.com";
+    public static final String DOMAIN_TEST = "http://localhost:5000";
+
+    public static final String CLOVADOMAIN = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/";
+
+    public static final String PREFERENCE_NAME = "alcoholRecipe_app";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String NICKNAME = "nickname";
+
+}
